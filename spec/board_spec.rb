0ad2c9c7b9820e951a0ec158599c61e01520496c
Carson-Key <keyc@oregonstate.edu>
2020-06-09 09:43:21 -0700

    Add token_at method test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,14 @@ require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
 
+  let(:midgame_board) {
+    Board.new({
+      top: {left: :o, middle: nil, right: nil},
+      middle: {left: nil, middle: :x, right: nil},
+      bottom: {left: nil, middle: nil, right: nil}
+    })
+  }
+
   it 'exists' do
     Board.new
   end
@@ -15,6 +23,10 @@ describe 'A Tic Tac Toe board' do
     board.place(:x, :top, :left)
 
     expect(board.locations[:top][:left]).to eq(:x)
+  end
+
+  it 'token_at method grabs correct token' do
+    expect(midgame_board.token_at(:top, :left)).to eq(:o)
   end
 
 end
